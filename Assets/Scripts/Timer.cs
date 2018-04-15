39d319ad4750696e85188daf1f542a34d95d890e
moko-ist <yukie.hr@gmail.com>
2018-04-15 15:26:28 +0900

    Import SampleStageRun package

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+	public static float time;
+
+	// Use this for initialization
+	void Start () {
+		time = 0;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Goal.goal == false) {
+			time += Time.deltaTime;
+		}
+
+		int timeSec = Mathf.FloorToInt (time);
+		Text timerText = GetComponent<Text> ();
+		timerText.text = "Time:" + timeSec;
+	}
+}
